e9bfd6ef1073ef81d7847657431654d4e17154df
Vishal <vishalkalal1197@gmail.com>
2025-09-22 12:44:37 +0530

    JavaBasics

---
+++ src/ex_02_Java_Basics/Employee.java
@@ -0,0 +1,34 @@
+package ex_02_Java_Basics;
+
+public class Employee {
+    //Variables
+    int eid;
+    String ename;
+    String job;
+    int sal;
+
+    //Methods
+    void display()
+    {
+        System.out.println(eid);
+        System.out.println(ename);
+        System.out.println(job);
+        System.out.println(sal);
+    }
+    public static void main(String[] args)
+    {
+        Employee emp1=new Employee();//Object
+        emp1.eid=101;
+        emp1.ename="John";
+        emp1.job="Manager";
+        emp1.sal=500000;
+        emp1.display();
+
+        Employee emp2=new Employee();//Object
+        emp2.eid=102;
+        emp2.ename="Johny";
+        emp2.job="Assistant Manager";
+        emp2.sal=50000;
+        emp2.display();
+    }
+}
