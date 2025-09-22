640ec849a546f0c7d54837e6f33e808107bc5cc2
Vishal <vishalkalal1197@gmail.com>
2025-09-22 12:45:05 +0530

    JavaBasics

---
+++ src/StringComparison.java
@@ -0,0 +1,13 @@
+public class StringComparison {
+    public static void main(String[] args){
+
+        //sc1
+        String s1=new String("welcome");
+        String s2="welcome";
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s1==s2);
+        System.out.println(s1.equals(s2));
+
+    }
+}
