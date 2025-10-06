6def2be10828ee1c05f9d50895e8ae389ce5b79d
Vishal <vishalkalal1197@gmail.com>
2025-10-06 20:41:25 +0530

    JavaBasics

---
+++ src/JavaPrograms/ArrayReverse.java
@@ -0,0 +1,12 @@
+package JavaPrograms;
+
+public class ArrayReverse {
+        public static void main(String[] args){
+        char s[]={'h','e','l','l','o'};
+
+        for(int i=s.length-1;i>=0;i--) {
+            System.out.print(s[i]);
+        }
+    }
+    }
+
