acfb92ca26114f29dfb112d7bb58b1c1ad82f230
Vishal <vishalkalal1197@gmail.com>
2025-10-28 12:46:08 +0530

    JavaBasics

---
+++ src/ForLoop/Prt_1_10.java
@@ -0,0 +1,10 @@
+package ForLoop;
+
+public class Prt_1_10 {
+    public static void main(String[] args) {
+        int n=10;
+        for(int i=1;i<=n;i++){
+            System.out.println(i);
+        }
+    }
+}
