de066502e2139e0811558a594b4f8ec347e8e6c1
Vishal <vishalkalal1197@gmail.com>
2025-10-28 23:37:40 +0530

    JavaBasics

---
+++ src/ForLoop/SumOfNaturalNumbers.java
@@ -0,0 +1,19 @@
+package ForLoop;
+import java.util.Scanner;
+
+public class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the number until which to find the sum of Natural numbers");
+        int n=scanner.nextInt();
+        int S=0;
+
+        for(int i=1;i<n;i++){
+            S=S+i;
+        }
+        System.out.println("The sum is :"+S);
+
+
+    }
+
+}
